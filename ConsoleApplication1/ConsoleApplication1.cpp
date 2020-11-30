9442d4e3daa7d37f65ac3793c7a8ab456ba4ed41
integral <volkovanton71@yandex.ru>
2020-11-30 13:05:03 +0300

    second

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "sjdklfjsdlkfjHello World!\n";
+    std::cout << "fddfdlkfjHello World!\n";
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
